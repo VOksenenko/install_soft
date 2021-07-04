2ee408ce584425af8e2efed96a18ff0669173b3a
Vlad Oksenenko <voksenenko@gmail.com>
2021-07-04 21:29:50 +0300

    Switch depending on OS

--- install_software.sh
+++ install_software.sh
@@ -21,3 +21,27 @@ else
         input+=("${line}")
     done < "$inputfile"
 fi
+
+# Get OS distribution  from /etc/os-release.
+# Filtered by ID, get  second tab after  equal sign using awk. Get rid of quotes using tr.
+OS=$( cat /etc/os-release|grep -iE '^ID='| awk -F '=' '{print $2}'| tr -d '"' )
+
+# For each software in input array install soft
+for soft in "${input[@]}"
+do
+    # Use different commands depend on OS distribution using case operator and possible cases.
+    # If neither ubuntu nor centos found, shows information message.
+        case $OS in
+        ubuntu)
+            sudo apt -y install $soft
+            ;;
+
+        centos)
+            sudo yum -y install $soft
+            ;;
+
+       *)  # This message will be shown if OS release do not match ubuntu or centos
+           echo  "Sorry. Cannot define OS distribution. Cannot install soft on this host."
+           ;;
+    esac
+done
